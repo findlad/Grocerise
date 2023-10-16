3efaf19520a6f7b23f1f134b9646806fb94ebe41
Findlad <findlad@gmail.com>
2023-10-16 15:11:23 -0600

    attempt scraping, no work on safeway superstore
    Co-authored-by: Elvin Limpin <elvinlimpin@gmail.com>

---
+++ index.js
@@ -0,0 +1,38 @@
+const productPage = "https://voila.ca/products/490731EA/details";
+const safewayMilk = "";
+// selector is the class (or whatever) of the data youre interested in. This will be different for each website
+const selector = ".$";
+
+const axios = require("axios");
+
+/* ... */
+async function getHTML(url) {
+  const { data: html } = await axios.get(url).catch((err) => {
+    console.log("Couldn't get the page ☹️");
+    //console.log(err);
+  });
+
+  console.log("it worked");
+  //console.log(html);
+  return html;
+}
+
+const cheerio = require("cheerio");
+/* ... */
+function scrapPrice(html) {
+  const $ = cheerio.load(html); //First you need to load in the HTML
+  const price = $(selector)
+    .text() // we get the text
+    .trim();
+  console.log("it worked too");
+  //console.log(price);
+  console.log(price.substring(0, 10));
+  return price;
+}
+
+async function init() {
+  const html = await getHTML(productPage);
+  scrapPrice(html);
+}
+
+init();
