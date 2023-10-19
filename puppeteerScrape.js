794e8f870622f42737ba36a7902ad130f477ddef
Findlad <findlad@gmail.com>
2023-10-19 15:07:28 -0600

    puppeteer progress

--- puppeteerScrape.js
+++ puppeteerScrape.js
@@ -1,11 +1,12 @@
 const puppeteer = require("puppeteer");
+const scrollPageToBottom = require("puppeteer-autoscroll-down");
 const cheerio = require("cheerio");
 
 (async () => {
   // Superstore
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  let displaySup = document.getElementById("supMilk");
+  //const displaySs = document.getElementById("supMilk");
   await page.goto(
     "https://www.realcanadiansuperstore.ca/milk-1-mf/p/20657990_EA"
   );
@@ -26,7 +27,7 @@ const cheerio = require("cheerio");
 
     const superstoreMilkPrice = scrapePrice(htmlCode);
     console.log("Superstore " + superstoreMilkPrice);
-    displaySup.value = superstoreMilkPrice;
+    //displaySs.innerHTML = superstoreMilkPrice;
   } catch (error) {
     console.error("Error: ", error);
   } finally {
@@ -44,18 +45,23 @@ const cheerio = require("cheerio");
   );
 
   try {
-    const textSelectorWm = ".inline-flex flex-column"; // Replace with the correct selector
-    await page.waitForSelector(textSelectorWm);
+    const textSelectorWm = "span.inline-flex flex-column"; // Replace with the correct selector
+    //await page.waitForSelector(".relative lh-title f6 gray pb3 dn");
+    //await scrollPageToBottom(page);
+    //await page.waitForNetworkIdle;
+    //await page.waitForNavigation({ waitUntil: "networkidle2" });
+    //await page.waitFor(5000);
+    await scrollPageToBottom(page);
 
     const htmlCode = await page.content();
 
     function scrapePrice(html) {
       const $ = cheerio.load(html);
-      const price = $(textSelectorWm).text().trim();
-      console.log("Walmart " + price.substring(0, 10));
-      return price;
+      const priceWm = $(textSelectorWm).text().trim();
+      console.log("Walmart " + priceWm.substring(0, 10));
+      return priceWm;
     }
-
+    console.log(htmlCode);
     const walmartMilkPrice = scrapePrice(htmlCode);
     console.log("walmart " + walmartMilkPrice);
   } catch (error) {
@@ -74,14 +80,15 @@ const cheerio = require("cheerio");
   try {
     const textSelectorSw = ".text__Text-sc-6l1yjp-0 sc-jOxuqd bIGwoI gJweLJ"; // Replace with the correct selector
     await page.waitForSelector(textSelectorSw);
+    //await page.waitForNavigation({ waitUntil: "networkidle2" });
 
     const htmlCode = await page.content();
 
     function scrapePrice(html) {
       const $ = cheerio.load(html);
-      const price = $(textSelectorSw).text().trim();
-      console.log("Safeway " + price.substring(0, 10));
-      return price;
+      const priceSw = $(textSelectorSw).text().trim();
+      console.log("Safeway " + priceSw.substring(0, 10));
+      return priceSw;
     }
 
     const SafewayMilkPrice = scrapePrice(htmlCode);
@@ -102,15 +109,15 @@ const cheerio = require("cheerio");
   try {
     const textSelectorNf =
       ".price__value selling-price-list__item__price selling-price-list__item__price--now-price__value";
-    await page.waitForSelector(textSelectorNf);
-
+    //await page.waitForSelector(textSelectorNf);
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
     const htmlCode = await page.content();
 
     function scrapePrice(html) {
       const $ = cheerio.load(html);
-      const price = $(textSelectorNf).text().trim();
+      const priceNf = $(textSelectorNf).text().trim();
 
-      return price;
+      return priceNf;
     }
 
     const noFrillsMilkPrice = scrapePrice(htmlCode);
@@ -133,14 +140,15 @@ const cheerio = require("cheerio");
   try {
     const textSelectorTt = ".productFullDetail-productPrice-Aod";
     await page.waitForSelector(textSelectorTt);
+    //await page.waitForNavigation({ waitUntil: "networkidle2" });
 
     const htmlCode = await page.content();
 
     function scrapePrice(html) {
       const $ = cheerio.load(html);
-      const price = $(textSelectorTt).text().trim();
+      const priceTt = $(textSelectorTt).text().trim();
 
-      return price;
+      return priceTt;
     }
 
     const tandtMilkPrice = scrapePrice(htmlCode);
