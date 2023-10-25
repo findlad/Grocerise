e9a28383fabe8e240704e2c65906ef4304d8a322
Findlad <findlad@gmail.com>
2023-10-24 21:21:03 -0600

    working on displaying the json filtered data

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 
-const localJsonFile = "priceHistory.json";
-const gridId;
-let gridId = document.getElementById(line.shop + line.item);
 
+let gridId;
 document.getElementById("loadData").addEventListener("click", function() {
-  // Load the JSON file using Fetch API
-  fetch("priceHistory.json")
-      .then(response => response.json())
-      .then(data => {
-          // Filter the entries by category
-          const category = "Milk"; // Change this to the category you want
-          const categoryEntries = data.filter(entry => entry.category === category);
-
-          // Find the newest entry in the filtered category
-          categoryEntries.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-          const newestEntry = categoryEntries[0];
-
-          // Display the result
-          document.getElementById("result").textContent = JSON.stringify(newestEntry, null, 2);
+    // Load the JSON file using Fetch API
+    fetch("priceHistory.json")
+    .then(response => response.json())
+    .then(data => {
+        // Filter the entries by category
+        const category = "milk"; // Change this to the category you want
+        const categoryEntries = data.filter(entry => entry.category === category);
+        
+        // Find the newest entry in the filtered category
+        categoryEntries.sort((a, b) => new Date(b.day) - new Date(a.day));
+        const newestEntry = categoryEntries[0];
+        
+        // Display the result
+        let gridId = newestEntry.shop + newestEntry.item;
+          document.getElementById(gridId).textContent = JSON.stringify(newestEntry, null, 2);
       })
       .catch(error => {
           console.error("Error loading JSON file:", error);
       });
-});
+};
